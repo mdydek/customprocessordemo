e670fa37673882c48e7f6a49120c0214da679cae
michal <dydmichal@gmail.com>
2025-07-16 14:56:06 +0200

    shared files structure

--- shared/NativeAudioProcessingModule.cpp
+++ shared/NativeAudioProcessingModule.cpp
@@ -1,23 +1,16 @@
 #include "NativeAudioProcessingModule.h"
+#include "MyProcessorNodeHostObject.h"
 #include <iostream>
-#include <audioapi/HostObjects/AudioContextHostObject.h>
-#include <audioapi/jsi/JsiHostObject.h>
-#include "CustomProcessorNodeHostObject.h"
 #include <functional>
 #include <memory>
+#include <audioapi/HostObjects/AudioContextHostObject.h>
+#include "MyProcessorNode.h"
 
 namespace facebook::react {
 
 NativeAudioProcessingModule::NativeAudioProcessingModule(std::shared_ptr<CallInvoker> jsInvoker)
     : NativeAudioProcessingModuleCxxSpec(std::move(jsInvoker)) {}
 
-    // jsi::Object NativeAudioProcessingModule::createCustomProcessor(jsi::Runtime &runtime,
-    //                                                                jsi::Value audioContextJSIValue) {
-    //   auto context = audioContextJSIValue.asObject(runtime).getHostObject<audioapi::BaseAudioContextHostObject>(runtime);
-    //   auto node = std::make_shared<audioapi::MyProcessorNode>(context->context_.get());
-    //   auto nodeHostObject = std::make_shared<audioapi::CustomProcessorNodeHostObject>(node);
-    //   return jsi::Object::createFromHostObject(runtime, nodeHostObject);
-    // }
 void NativeAudioProcessingModule::injectCustomProcessorInstaller(jsi::Runtime &runtime) {
   auto installer = createInstaller(runtime);
   runtime.global().setProperty(runtime, "createCustomProcessorNode", installer);
@@ -30,11 +23,12 @@ jsi::Function NativeAudioProcessingModule::createInstaller(jsi::Runtime &runtime
       0,
       [](jsi::Runtime &runtime, const jsi::Value &thisVal, const jsi::Value *args, size_t count) {
           auto object = args[0].getObject(runtime);
-          auto context = object.getHostObject<jsi::HostObject>(runtime);
-          auto ctx2 = std::static_pointer_cast<audioapi::AudioContextHostObject>(context);
-        if (ctx2 != nullptr) {
-          auto node = std::make_shared<audioapi::MyProcessorNode>(ctx2->context_.get());
-          auto nodeHostObject = std::make_shared<audioapi::AudioNodeHostObject>(node);
+          auto ctx = object.getHostObject<jsi::HostObject>(runtime);
+          auto context = std::static_pointer_cast<audioapi::AudioContextHostObject>(ctx);
+//        auto context = object.getHostObject<audioapi::AudioContextHostObject>(runtime);
+        if (context != nullptr) {
+          auto node = std::make_shared<audioapi::MyProcessorNode>(context->context_.get());
+          auto nodeHostObject = std::make_shared<audioapi::MyProcessorNodeHostObject>(node);
           return jsi::Object::createFromHostObject(runtime, nodeHostObject);
         }
         return jsi::Object::createFromHostObject(runtime, nullptr);
